f0cd90d0f8fcb0c88ffeb8ade77f1c0f6bb217e4
Satkar Wagle <satkarw@gmail.com>
2025-01-29 11:01:15 +0545

    no changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 
-import SignUp from "./login/page";
+
 
 export default function Home() {
   return (
     <div>
 
-      <SignUp/>
-
-
+        
 
     </div>
   );
